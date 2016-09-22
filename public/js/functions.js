3a3eddb04cf5d0fa4b79984a3d5ddcdc40910750
guilhermeiobbi <user@V0000954.vectoritcgroup.com>
2016-09-22 15:21:50 -0300

    added new layout and major fixes

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,18 +1,15 @@
 $(function(){
 
     $(".dropdown-menu li a").click(function(){
-      $(".btn:first-child").text($(this).text());
-      $(".btn:first-child").val($(this).text());
-      $("#inputRegion").val($(this).text());
-   });
-
-   $('#searchButton').on('click', function () {
-    if($('#inputName').val() != '') {
-        var $btn = $(this).button('loading')
-    }
-    // business logic...
-    //$btn.button('reset')
-  })
+        $(".btn:first-child").text($(this).text());
+        $(".btn:first-child").val($(this).text());
+        $("#inputRegion").val($(this).text());
+    });
+    
+    $('#myForm').submit(function (e) {
+        $('#searchIcon').addClass('fa fa-spinner fa-spin fa-1x fa-fw');
+        $('#searchButton').prop('disabled', true);
+    });
 
 });
 
